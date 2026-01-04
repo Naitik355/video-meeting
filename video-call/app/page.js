a87fe792f34b364f1bf94ccf890ad3317943c958
Naitik Aggarwal <aggarwalnaitik993@gmail.com>
2026-01-04 13:38:15 +0530

    Update page.js

--- video-call/app/page.js
+++ video-call/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 text-white">
       <div className="p-8 bg-gray-800 rounded-2xl border border-gray-700 w-80 shadow-2xl">
         <h2 className="text-xl font-semibold mb-4 text-center">Enter your name</h2>
-        <input placeholder="e.g. Naitik" className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray text-white" value={username} onChange={(e)=>setUsername(e.target.value)}></input>
+        <input className="px-4 py-3 w-full rounded-lg bg-gray-700/80 border border-gray text-white" value={username} onChange={(e)=>setUsername(e.target.value)}></input>
         <button onClick={handleJoin} className="mt-5 w-full py-3 bg-blue-600 hover:bg-blue-700 rounded-lg font-medium">Join Meeting</button>
       </div>
     </div>
